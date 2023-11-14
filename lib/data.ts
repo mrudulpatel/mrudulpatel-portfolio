a20bcca7065144f75e700c4e9d385b7f0da33019
Mrudul Patel <84196599+mrudulpatel@users.noreply.github.com>
2023-11-14 10:22:16 +0000

    updated experiences

--- lib/data.ts
+++ lib/data.ts
@@ -34,30 +34,22 @@ export const links = [
 ] as const
 
 export const experiencesData = [
+    
     {
-      title: "Graduated bootcamp",
-      location: "Miami, FL",
+      title: "Full Stack Developer Internship",
+      location: "Fair Share IT Services Pvt. Ltd.",
       description:
-        "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-      icon: React.createElement(LuGraduationCap),
-      date: "2019",
-    },
-    {
-      title: "Front-End Developer",
-      location: "Orlando, FL",
-      description:
-        "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+        "I worked as a full stack developer",
       icon: React.createElement(CgWorkAlt),
-      date: "2019 - 2021",
+      date: "01-08-2022 - 31-10-2022",
     },
     {
-      title: "Full-Stack Developer",
-      location: "Houston, TX",
-      description:
-        "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-      icon: React.createElement(FaReact),
-      date: "2021 - present",
-    },
+      title: "Full Stack Developer Internship",
+      location: "MSEDCL, Rasta Peth, Pune",
+      description: "Developed a Portal for managing the accountability of Tools & Parts provided to the technical engineers at MSEDCL, Rasta Peth, Pune.",
+      icon: React.createElement(CgWorkAlt),
+      date: "July 2023 - Dec 2023",
+    }
   ] as const;
   
   export const projectsData = [
