4baac2927b48127aadd42265d8e2a9411e62a3d5
Mrudul Patel <84196599+mrudulpatel@users.noreply.github.com>
2023-12-12 14:50:59 +0000

    Update CV download link

--- components/intro.tsx
+++ components/intro.tsx
@@ -85,7 +85,7 @@ const Intro = () => {
 
         <Link
           download={true}
-          href="/CV.pdf"
+          href="/MrudulResume.pdf"
           className="group dark:bg-white/10 bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition border border-black/10"
         >
           Download CV{" "}
